9c921674549b0d93b69c50751fef022464dc90ff
BAUu <yimej07@gmail.com>
2024-04-23 17:40:15 +0900

    feat: main_screen 만들기

---
+++ lib/presentation/screen/main_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff1D1E23),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 32,
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xff959EA2),
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                width: 340,
+                height: 120,
+                child: Center(
+                  child: Text(
+                    'Workout Routine Timer',
+                    style: TextStyle(fontSize: 32, fontFamily: 'April16thTTF-Safety'),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xff959EA2),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              width: 340,
+              height: 240,
+              child: Column(
+                children: [
+                  Text(
+                    'Timer',
+                    style: TextStyle(fontSize: 40, fontFamily: 'April16thTTF-Safety'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: 32,),
+          Container(
+            width: 360,
+            height: 360,
+            // margin: EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Color(0xff525E66),
+              borderRadius: BorderRadius.circular(360 / 2),
+            ),
+            child: TextButton(
+              onPressed: () {
+                // 버튼이 눌렸을 때 수행할 작업을 여기에 추가하세요.
+              },
+              child: Text(
+                'Start',
+                style: TextStyle(fontSize: 80, color: Colors.black,fontFamily: 'April16thTTF-Promise'),
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
